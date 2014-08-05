c09e66b19dcc163cd61ddc1562075085ada978b1
Shruti Sharma <shrutijalewar@gmail.com>
2014-08-05 10:38:40 -0500

    refactoring template

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,6 +2,7 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
 var home = require('../controllers/home');
 var items = require('../controllers/items');
 
@@ -9,7 +10,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
-
+  app.use(methodOverride());
   app.get('/', home.index);
   app.get('/about', home.about);
   app.get('/faq', home.faq);
@@ -18,7 +19,7 @@ module.exports = function(app, express){
   app.post('/items', items.create);
   app.get('/items', items.index);
   app.get('/items/:id', items.show);
-  app.post('/items/:id/delete', items.destroy);
+  app.delete('/items/:id', items.destroy);
 
   console.log('Pipeline Configured');
 };
